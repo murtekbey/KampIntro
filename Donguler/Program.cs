9e7e6c7111d18b8ab8f7ca5e275a274e07a72be8
Murat Altınpınar <murtekbey@gmail.com>
2021-01-14 00:45:51 +0300

    2. Ders Eğitim Sonu

---
+++ Donguler/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya Başlangıç İçin Temel Kurs";
+            string kurs3 = "Java";
+            string kurs4 = "Python";
+            string kurs5 = "C++";
+
+            Console.WriteLine(kurs1);
+            Console.WriteLine(kurs2);
+            Console.WriteLine(kurs3);
+            Console.WriteLine(kurs4);
+            Console.WriteLine(kurs5);
+
+            // array dizi
+
+            string[] kurslar = new string[] {
+            "Yazılım Geliştirici Yetiştirme Kampı",
+            "Programlamaya Başlangıç İçin Temel Kurs",
+            "Java",
+            "Python",
+            "C++"
+            };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa Sonu - Footer");
+        }
+    }
+}
--- KampIntro/Program.cs
+++ KampIntro/Program.cs
@@ -13,6 +13,21 @@ namespace KampIntro
             int ogrenciSayisi = 32000;
             double faizOrani = 1.45;
             bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
 
             if (sistemeGirisYapmisMi == true)
             {
