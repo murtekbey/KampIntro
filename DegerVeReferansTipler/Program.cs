5916cc0f00d10d42e05866aa8b4606aef0c9be7f
Murat Altınpınar <murtekbey@gmail.com>
2021-01-17 01:49:39 +0300

    Değer ve referans tipler

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -12,13 +12,13 @@ namespace DegerVeReferansTipler
             int sayi2 = 30;
             sayi1 = sayi2;
             sayi2 = 65;
-            //sayi1 ??
+            //sayi1 = 30
 
             int[] sayilar1 = new int[] { 10, 20, 30 };
             int[] sayilar2 = new int[] { 100, 200, 300 };
             sayilar1 = sayilar2;
             sayilar2[0] = 999;
-            //sayilar1[0]
+            //sayilar1[0] = 999
 
         }
     }
