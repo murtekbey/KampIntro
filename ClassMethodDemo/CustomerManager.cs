96e23b5f4efba25ff08ed8924f86c5b802cd135a
Murat Altınpınar <murtekbey@gmail.com>
2021-01-17 19:12:23 +0300

    ClassMethodDemo ödevi eklendi.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " isimli kişi müşteri listenize eklendi.");
+        }
+
+        public void Remove(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " isimli kişi müşteri listenizden silinmiştir.");
+        }
+
+        public void Get(Customer customer)
+        {
+            Console.WriteLine("Id: " + customer.Id + ", Ad: " + customer.Name + ", Soyad: " + customer.Surname);
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Murat";
+            customer1.Surname = "Altınpınar";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Süleyman";
+            customer2.Surname = "Yaka";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.Name = "Anıl";
+            customer3.Surname = "Koralay";
+
+            Customer customer4 = new Customer();
+            customer4.Id = 4;
+            customer4.Name = "Mustafa";
+            customer4.Surname = "Gündoğdu";
+
+            Customer customer5 = new Customer();
+            customer5.Id = 5;
+            customer5.Name = "Mustafa";
+            customer5.Surname = "Çağatay";
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3, customer4, customer5 };
+
+            CustomerManager customerManager = new CustomerManager();
+
+            foreach (Customer customer in customers)
+            {
+                customerManager.Add(customer); // Ekle
+                customerManager.Remove(customer); // Sil
+                customerManager.Get(customer); // Listele
+                Console.WriteLine("----------------------------");
+            }
+        }
+    }
+}
