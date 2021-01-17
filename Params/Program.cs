16eef23cddb9786efadb4a9b5925f40081ba2c29
Murat Altınpınar <murtekbey@gmail.com>
2021-01-17 19:27:45 +0300

    Params eklendi.

---
+++ Params/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq; // Sum metodu kullanabilmek için import etmelisiniz.
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(1, 2, 3, 4, 5)); // tüm sayıları toplar ve sonucu yazdırır.
+        }
+
+        static int Add(params int[] numbers) // istediğin kadar integer değer gönderebilirsin.
+        {
+            return numbers.Sum(); // Sum() methodu dizi içerisinde olan bütün sayıları toplar.
+        }
+    }
+}
