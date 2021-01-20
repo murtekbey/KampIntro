fa13435e45201f90c9cf8f657091451b55d1d421
Murat Altınpınar <murtekbey@gmail.com>
2021-01-20 23:05:16 +0300

    OOP1 eklendi.

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+    }
+}
